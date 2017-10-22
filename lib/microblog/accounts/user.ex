08280d68c2b8ade81a8f681e574d99b94fbf01bb
christinetchen <chen.chr@husky.neu.edu>
2017-10-22 17:05:11 -0400

    Add password functionality

--- lib/microblog/accounts/user.ex
+++ lib/microblog/accounts/user.ex
@@ -3,11 +3,18 @@ defmodule Microblog.Accounts.User do
   import Ecto.Changeset
   alias Microblog.Accounts.User
 
-
+# Attribution: Prof. Nat Tucks' lecture notes on passwords & comeonin doc
   schema "users" do
     field :display_name, :string
     field :email, :string
     field :name, :string
+
+    field :password_hash, :string
+    field :pw_tries, :integer
+    field :pw_last_try, :utc_datetime
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
@@ -15,7 +22,33 @@ defmodule Microblog.Accounts.User do
   @doc false
   def changeset(%User{} = user, attrs) do
     user
-    |> cast(attrs, [:name, :display_name, :email])
-    |> validate_required([:name, :display_name, :email])
+    |> cast(attrs, [:name, :display_name, :email, :password, :password_confirmation])
+    |> validate_confirmation(:password)
+    |> validate_password(:password)
+    |> put_pass_hash()
+    |> validate_required([:name, :display_name, :email, :password_hash])
   end
+
+  # Password validation
+  def validate_password(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, password ->
+      case valid_password?(password) do
+        {:ok, _} -> []
+        {:error, msg} -> [{field, options[:message] || msg}]
+      end
+    end)
+  end
+
+  def put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
+  def put_pass_hash(changeset), do: changeset
+
+  def valid_password?(password) when byte_size(password) > 7 do
+    {:ok, password}
+  end
+
+  def valid_password?(_), do: {:error, "The password is too short"}
+
 end
