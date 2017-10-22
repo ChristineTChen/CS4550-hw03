66981cf186b494736afa1ff5d2b191f4fc61133c
christinetchen <chen.chr@husky.neu.edu>
2017-10-22 17:21:18 -0400

    Add user authentication to session_controller

--- lib/microblog_web/controllers/session_controller.ex
+++ lib/microblog_web/controllers/session_controller.ex
@@ -3,8 +3,17 @@ defmodule MicroblogWeb.SessionController do
 
   alias Microblog.Accounts
 
-  def login(conn, %{"email" => email}) do
+# Attribution: Prof. Nat Tuck's password lecture notes
+  def get_and_auth_user(email, password) do
     user = Accounts.get_user_by_email(email)
+    case Comeonin.Argon2.check_pass(user, password) do 
+      {:ok, user} -> user
+      _else       -> nil
+    end
+  end
+
+  def login(conn, %{"email" => email, "password" => password}) do
+    user = get_and_auth_user(email, password)
 
     if user do
 #      post = conn.assigns[:current_post]
@@ -12,12 +21,12 @@ defmodule MicroblogWeb.SessionController do
 
       conn
       |> put_session(:user_id, user.id)
-      |> put_flash(:info, "Logged in as #{user.email}")
+      |> put_flash(:info, "Logged in as #{user.name}")
       |> redirect(to: post_path(conn, :index))
     else
       conn
       |> put_session(:user_id, nil)
-      |> put_flash(:error, "No such user")
+      |> put_flash(:error, "Error in email/password")
       |> redirect(to: post_path(conn, :index))
     end
   end
@@ -28,5 +37,33 @@ defmodule MicroblogWeb.SessionController do
      |> put_flash(:info, "Logged out.")
      |> redirect(to: post_path(conn, :index))
   end
+
+  def update_tries(throttle, prev) do
+    if throttle do
+      prev + 1
+    else
+      1
+    end
+  end
+
+  def throttle_attempts(user) do 
+    y2k = DateTime.from_naive!(~N[2000-01-01 00:00:00], "Etc/UTC")
+    prv = DateTime.to_unix(user.pw_last_try || y2k)
+    now = DateTime.to_unix(DateTime.utc_now())
+    thr = (now - prv) < 3600
+
+    if (thr && user.pw_tries > 5) do 
+      nil
+    else 
+      changes = %{
+        pw_tries: update_tries(thr, user.pw_tries),
+        pw_last_try: DateTime.utc_now(),
+      }
+      IO.inspect(user)
+      {:ok, user} = Ecto.Changeset.cast(user, changes, [:pw_tries, :pw_last_try])
+      |> Microblog.Repo.update 
+      user
+    end
+  end 
 end
 
